5173aa83a9750e28f92cc79c766284ed53ed4ef9
preeti13456 <preetijecrc99@outlook.com>
2020-06-10 00:43:14 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/dropdownmenu/drop_down_menu/MainActivity.kt
@@ -0,0 +1,6 @@
+package dropdownmenu.drop_down_menu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:drop_down_menu/show_dialog_drop_mwnu/dropdown.dart';
+
+//void main() => runApp(new BottomNavigationHangAbove());
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+//      home: new AppBarBottom(),
+//        home: DropdownMenu(),
+    );
+  }
+}
+
+class ExpendableExample {}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
